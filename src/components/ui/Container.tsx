dcc78b437d5e187975abb070f5af1aec1b9ac75e
Petr Bulánek <bulanek.petr@gmail.com>
2024-12-13 17:03:53 +0100

    fix: artifact container width

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -2,9 +2,24 @@ import clsx from 'clsx';
 import { PropsWithChildren } from 'react';
 
 interface Props {
+  size?: 'full';
   className?: string;
 }
 
-export function Container({ className, children }: PropsWithChildren<Props>) {
-  return <div className={clsx('container', className)}>{children}</div>;
+export function Container({
+  size,
+  className,
+  children,
+}: PropsWithChildren<Props>) {
+  return (
+    <div
+      className={clsx(
+        'container',
+        { 'max-w-none': size === 'full' },
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 }
